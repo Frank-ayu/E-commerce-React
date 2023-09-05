b52f8fcbd26a956b8eb8ba3fd9babd8b0348bec5
Fenwei <2074305892@qq.com>
2023-09-05 11:19:21 +1000

    0905_1
    add image link

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
 const Slider = () => {
+
+    const data = [
+        "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    ];
+
     return (
-        <div className="slider"></div>
+        <div className="slider">
+            <div className="container">
+                <img src={data[0]} />
+                <img src={data[1]} />
+                <img src={data[2]} />
+            </div>
+            <div className="icons">
+                <div className="icon"></div>
+                <div className="icon"></div>
+            </div>
+        </div>
     )
 }
 
